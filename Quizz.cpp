bc3acba4e71d52207b6c96d2258f65ccd9c60a78
onezer00 <caimbebr@gmail.com>
2017-09-09 23:50:33 -0300

    Update Quizz.cpp

--- Quizz.cpp
+++ Quizz.cpp
@@ -1,22 +1,32 @@
 #include <iostream>
-#include <stdio.h>
 
 /**
-By: OneZer0 - Quizz!!!
+Author: OneZer0 - Quizz
 **/
 
 using namespace std;
+void limpar(){
+    #ifdef linux
+        system("clear");
+    #else
+        system("cls");
+    #endif // linux
+}
+
 void logo(){
-    cout << "  ______             __                     " << endl;
-    cout << " /      \           /  |                    " << endl;
-    cout << "/$$$$$$  | __    __ $$/  ________  ________ " << endl;
-    cout << "$$ |  $$ |/  |  /  |/  |/        |/        |" << endl;
-    cout << "$$ |  $$ |$$ |  $$ |$$ |$$$$$$$$/ $$$$$$$$/ " << endl;
-    cout << "$$ |_ $$ |$$ |  $$ |$$ |  /  $$/    /  $$/  " << endl;
-    cout << "$$ / \$$ |$$ \__$$ |$$ | /$$$$/__  /$$$$/__ " << endl;
-    cout << "$$ $$ $$< $$    $$/ $$ |/$$      |/$$      |" << endl;
-    cout << " $$$$$$  | $$$$$$/  $$/ $$$$$$$$/ $$$$$$$$/ " << endl;
-    cout << "     $$$/                                   " << endl;
+    cout << "* -----------------------------------------------*"  << endl;
+    cout << "|    ______             __                       |" << endl;
+    cout << "|   /      \           /  |                       |" << endl;
+    cout << "|  /$$$$$$  | __    __ $$/  ________  ________   |" << endl;
+    cout << "|  $$ |  $$ |/  |  /  |/  |/        |/        |  |" << endl;
+    cout << "|  $$ |  $$ |$$ |  $$ |$$ |$$$$$$$$/ $$$$$$$$/   |" << endl;
+    cout << "|  $$ |_ $$ |$$ |  $$ |$$ |  /  $$/    /  $$/    |" << endl;
+    cout << "|  $$ / \$$ |$$ \__$$ |$$ | /$$$$/__  /$$$$/__     |" << endl;
+    cout << "|  $$ $$ $$< $$    $$/ $$ |/$$      |/$$      |  |" << endl;
+    cout << "|   $$$$$$  | $$$$$$/  $$/ $$$$$$$$/ $$$$$$$$/   |" << endl;
+    cout << "|       $$$/                                     |" << endl;
+    cout << "* -----------------------------------------------*"  << endl;
+    cout << "\n\n";
     cout << "\n\n";
 }
 
@@ -24,57 +34,77 @@ void menu(){
     int men, level, pontuacao = 0;
     logo();
 
-    cout << "1 - Escolher Level" << endl;
-    cout << "2 - Iniciar" << endl;
-    cout << "3 - Maior Pontuação" << endl;
-    cout << "4 - Sair" << endl;
+    cout << "* -----------------------------------------------*" << endl;
+    cout << "|    1 - Escolher Level                          |" << endl;
+    cout << "|    2 - Iniciar                                 |" << endl;
+    cout << "|    3 - Maior Pontuação                         |" << endl;
+    cout << "|    4 - Sair                                    |" << endl;
+    cout << "* -----------------------------------------------*" << endl;
     cout << "Entre com o número correspondente: ";
 
     cin >> men;
 
     switch(men){
     case 1:
-        system("clear || cls");
-        cout << "1 - Iniciante" << endl;
-        cout << "2 - Médio" << endl;
-        cout << "3 - Avançado" << endl;
-        cout << "4 - Gênio" << endl;
-        cout << "Escolha seu nível: ";
+        limpar();
+        cout << "* -----------------------------------------------*" << endl;
+        cout << "|  1 - Iniciante                                 |" << endl;
+        cout << "|  2 - Médio                                     |" << endl;
+        cout << "|  3 - Avançado                                  |" << endl;
+        cout << "|  4 - Gênio                                     |" << endl;
+        cout << "* -----------------------------------------------*"  << endl;
+        cout << "   Escolha seu nível: ";
+
 
         cin >> level;
 
-        system("clear");
+        limpar();
         switch(level){
             case 1:
-                cout << "O jogo foi alterado para o nível: Iniciante\n\n";
+                cout << "* -----------------------------------------------*"  << endl;
+                cout << "   O jogo foi alterado para o nível: Iniciante" << endl;
+                cout << "* -----------------------------------------------*"  << endl;
+                cout << "\n\n";
 
                 return menu();
             case 2:
-                cout << "O jogo foi alterado para o nível: Médio\n\n";
+                cout << "* -----------------------------------------------*"  << endl;
+                cout << "   O jogo foi alterado para o nível: Médio" << endl;
+                cout << "* -----------------------------------------------*"  << endl;
+                cout << "\n\n";
 
                 return menu();
             case 3:
-                cout << "O jogo foi alterado para o nível: Avançado\n\n";
+                cout << "* -----------------------------------------------*"  << endl;
+                cout << "   O jogo foi alterado para o nível: Avançado" << endl;
+                cout << "* -----------------------------------------------*"  << endl;
+                cout << "\n\n";
 
                 return menu();
             case 4:
-                cout << "O jogo foi alterado para o nível: Gênio\n\n";
+                cout << "* -----------------------------------------------*"  << endl;
+                cout << "   O jogo foi alterado para o nível: Gênio" << endl;
+                cout << "* -----------------------------------------------*"  << endl;
+                cout << "\n\n";
 
                 return menu();
             default:
-                cout << "Você digitou um número inválido...\n";
-                cout << "Voltando para o Menu principal!\n\n";
+                cout << "* -----------------------------------------------*"  << endl;
+                cout << "   Você digitou um número inválido!" << endl;
+                cout << "   Voltando para o Menu principal..." << endl;
+                cout << "* -----------------------------------------------*"  << endl;
+                cout << "\n\n";
 
             return (menu());
         }
 
     case 2:
-        system("clear");
+        limpar();
         cout << "Em construção...\n\n";
 
         return (menu());
     case 3:
-        system("clear");
+        limpar();
         cout << "Sua maior pontuação foi: ";
         cout << pontuacao << "\n\n";
 
@@ -82,7 +112,7 @@ void menu(){
     case 4:
         exit(1);
     default:
-        system("clear");
+        limpar();
         cout << "Você selecionou um número inválido, tente novamente: \n\n";
 
         return menu();
